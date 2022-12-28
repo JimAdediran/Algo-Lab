983c6997b8c0af5702b60ef84ab7c39fbb9ea651
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-12-28 07:10:13 -0500

    iniated algorithm two

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,28 @@
 #Algorithm to determine odd or even number
-num = int(input("Enter a number"))
-mod = num % 2
-print(mod == 0)
+
+"""def determine_odd_even():
+    num = int(input("Enter a number"))
+    mod = num % 2
+    print(mod == 0)
+
+new_number = determine_odd_even()"""
+
+#Algorithm task 2 less than 100
+
+array = [int(input("enter list"))]
+for x in array:
+    if x >= 100:
+        print(False)
+        break
+    else:
+        print(True)
+
+
+
+
+
+
+
+
+
 
