3df796e0f3e3acb35177803c38332aabe082ce8f
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-12-31 10:27:56 -0500

    Algorithm task 3 completed

--- algorithms.py
+++ algorithms.py
@@ -5,17 +5,63 @@
     mod = num % 2
     print(mod == 0)
 
-new_number = determine_odd_even()"""
+new_number = determine_odd_even()
 
 #Algorithm task 2 less than 100
 
-array = [int(input("enter list"))]
-for x in array:
-    if x >= 100:
-        print(False)
-        break
-    else:
-        print(True)
+def list_check ():
+      
+    for x in list:
+        if x >= check:
+            print(False)
+            break
+        else:
+            print(True)
+
+list = [10, 30, 80, 30]
+check = 100 
+
+checking_list = list_check()
+
+
+#Algorithm task 3 Repeated Names
+
+def repeated_names():
+    for i in range(0, len(list)):
+        for j in range(i+1, len(list)):
+            if(list[i] == list[j]):
+                print(True)
+            else:
+                print(False)"""
+list = ["John", "Kate", "John", "David"]
+        
+def repeat_name():
+
+    for i in range(0, len(list)):
+        for j in range(i+1, len(list)):
+            if(list[i] == list[j]):
+                print(True)
+                break
+            else:
+                print(False)
+
+
+
+name_check = repeat_name()
+
+
+
+#run_name = repeated_names()
+
+
+
+
+
+
+
+
+  
+
 
 
 
